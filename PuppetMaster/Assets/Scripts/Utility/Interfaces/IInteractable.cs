8cc42c876f1b5ae7a0ed299b5913bb40e169eeb7
onesketchyguy <lowebros9812@gmail.com>
2021-02-18 22:34:42 -0800

    Added some more asset packs, and did code review on weapon pickup

--- PuppetMaster/Assets/Scripts/Utility/Interfaces/IInteractable.cs
+++ PuppetMaster/Assets/Scripts/Utility/Interfaces/IInteractable.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 namespace PuppetMaster
 {
+    /// <summary>
+    /// Interface for an interactable object.
+    /// </summary>
     public interface IInteractable
     {
+        /// <summary>
+        /// Interacts with this object.
+        /// </summary>
+        /// <param name="sender"></param>
         public void Interact(GameObject sender);
     }
 }
--- PuppetMaster/Assets/Scripts/WeaponPickup.cs
+++ PuppetMaster/Assets/Scripts/WeaponPickup.cs
@@ -2,44 +2,69 @@ using UnityEngine;
 
 namespace PuppetMaster
 {
+    /// <summary>
+    /// A pickup object which requires a base weapon in order to work.
+    /// </summary>
     public class WeaponPickup : MonoBehaviour, IInteractable
     {
+        [Tooltip("The weapon to pickup once in range of this object.")]
         [SerializeField] private BaseWeapon weapon = null;
-        [SerializeField] private float turnSpeed = 30;
 
-        private Transform _tran;
+        [Tooltip("The speed at which to rotate around the 'z' axis.")]
+        [SerializeField] private float rotateSpeed = 30;
 
+        /// <summary>
+        /// A cached transform.
+        /// </summary>
+        private Transform _transform;
+
+        /// <summary>
+        /// Returns true if this object has a parent.
+        /// </summary>
+        /// <returns></returns>
         private bool GetBeingHeld()
         {
-            return transform.parent != null;
+            return _transform != null && _transform.parent != null;
         }
 
+        /// <summary>
+        /// Called once every time this component needs validation within the editor.
+        /// </summary>
         private void OnValidate()
         {
-            if (weapon == null)
-            {
-                weapon = GetComponent<BaseWeapon>();
-            }
+            // If possible try setting the weapon property automatically.
+            if (weapon == null) weapon = GetComponent<BaseWeapon>();
         }
 
         private void Start()
         {
-            _tran = transform;
+            // Cache the transform.
+            _transform = transform;
         }
 
         private void Update()
         {
-            var beingHeld = GetBeingHeld();
-            if (beingHeld) return;
+            // Don't rotate if currently being held.
+            if (GetBeingHeld() == true) return;
 
-            _tran.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+            // Rotate around the 'z' axis based on the frame rate.
+            _transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
         }
 
+        /// <summary>
+        /// Picks this weapon up.
+        /// </summary>
+        /// <param name="sender"></param>
         public void Interact(GameObject sender)
         {
+            // Try to get the combat manager component from the sender.
             var picker = sender.GetComponent<CombatManager>();
 
-            picker.SwapWeapon(weapon);
+            if (picker != null)
+            {
+                // Try to equip this weapon.
+                picker.SwapWeapon(weapon);
+            }
         }
     }
 }
